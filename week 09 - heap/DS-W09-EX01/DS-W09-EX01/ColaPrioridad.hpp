aef0a2e9748552217dab183b3ab86da2e319c203
abrahamayquipa <abrahamayquipa2000@gmail.com>
2023-10-28 16:24:50 -0500

    feat: add DS-W09-EX01

---
+++ week 09 - heap/DS-W09-EX01/DS-W09-EX01/ColaPrioridad.hpp
@@ -0,0 +1,88 @@
+﻿#ifndef __COLA_PRIORIDAD_HPP__
+#define __COLA_PRIORIDAD_HPP__
+#include <iostream>
+using namespace std;
+
+class colaPrioridad {
+public:
+    colaPrioridad(int* A, int n){
+        for(int i = 0;i < n;i++) {
+            arreglo[i] = A[i];
+        }
+        tamano = n;
+        buildMaxHeap(arreglo, tamano);
+    }
+
+    void insert(int valor) {
+        tamano++;
+        arreglo[tamano - 1] = valor;
+        int i = tamano - 1;
+        while (i > 0 && arreglo[parent(i)] < arreglo[i]) {
+            swap(arreglo[i], arreglo[parent(i)]);
+            i = parent(i);
+        }
+    }
+
+    int maximum(void) {
+        if (tamano <= 0) {
+            cerr << "heap overflow" << endl;
+            return -1;
+        }
+        return arreglo[0];
+    }
+
+    int extract_max(void) {
+        if (tamano <= 0) {
+            cerr << "heap overflow" << endl;
+            return -1;
+        }
+        int max = arreglo[0];
+        arreglo[0] = arreglo[tamano - 1];
+        tamano--;
+        maxHeapify(arreglo, tamano, 0);
+        return max;
+    }
+
+    void increase_key(int i, int x) {
+        arreglo[i] = max(arreglo[i], x);
+
+        while (i > 0 && arreglo[parent(i)] < arreglo[i]) {
+            swap(arreglo[parent(i)], arreglo[i]);
+            i = parent(i);
+        }
+    }
+private:
+    int arreglo[10000];
+    int tamano;
+
+    int parent(int i) { return (i - 1) / 2; }
+    int left(int i) { return 2 * i + 1; }
+    int right(int i) { return 2 * i + 2; }
+
+    void maxHeapify(int A[], int n, int i) {
+        int l = left(i);
+        int r = right(i);
+        int largest(0);
+        if (l <= (n - 1) && A[l] > A[i]) largest = l;
+        else largest = i;
+        if (r <= (n - 1) && A[r] > A[largest]) largest = r;
+        if (largest != i) {
+            swap(A[i], A[largest]);
+            maxHeapify(A, n, largest);
+        }
+    }
+
+    void buildMaxHeap(int A[], int n) {
+        for (int i = n / 2 - 1; i >= 0; i--) maxHeapify(A, n, i);
+    }
+
+    void heapsort(int A[], int n) {
+        buildMaxHeap(A, n);
+        for (int i = n - 1; i > 0; i--) {
+            swap(A[0], A[i]);
+            maxHeapify(A, --n, 0);
+        }
+    }
+};
+
+#endif
---
+++ week 09 - heap/DS-W09-EX01/DS-W09-EX01/DS-W09-EX01.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<cstdio>
+#include<list>
+#include<vector>
+#include<unordered_map>
+#include<climits>
+#include<unordered_set>
+#include<map>
+#include<set>
+#include<stack>
+#include "ColaPrioridad.hpp"
+
+int main() {
+    int arreglo[10] = { 10,9,8,7,6,50,4,3,2,1 };
+    colaPrioridad objetoColaPrioridad(arreglo, 10);
+    cout << objetoColaPrioridad.maximum() << endl;
+
+    objetoColaPrioridad.insert(20);
+    objetoColaPrioridad.insert(10);
+    cout << objetoColaPrioridad.maximum() << endl;
+
+    objetoColaPrioridad.extract_max();
+    cout << objetoColaPrioridad.maximum() << endl;
+
+    objetoColaPrioridad.increase_key(0, 5);
+    cout << objetoColaPrioridad.maximum() << endl;
+
+    objetoColaPrioridad.extract_max();
+    cout << objetoColaPrioridad.maximum() << endl;
+
+    objetoColaPrioridad.increase_key(1, 30);
+    cout << objetoColaPrioridad.maximum() << endl;
+
+    return 0;
+}
